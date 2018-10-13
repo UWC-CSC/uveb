4f38df9928783cc507566f51270567d1c7acf7ac
Marko Vejnovic <marko.vejnovic@hotmail.com>
2018-10-13 19:44:10 +0800

    Cleaned up schema

--- uveb/schema.sql
+++ uveb/schema.sql
@@ -2,7 +2,7 @@ USE uveb;
 
 -- Create c_videos table
 CREATE TABLE c_videos (
-	c_videos_id smallint unsigned not null auto_increment,
+	id smallint unsigned not null auto_increment,
 
 	title varchar(255) not null,
 	description varchar(1024),
@@ -12,15 +12,15 @@ CREATE TABLE c_videos (
 	uri varchar(2083),
 	path varchar(2083),
 	
-	constraint pk_c_videos primary key(c_videos_id)
+	constraint pk_c_videos primary key(id)
 );
 
 -- Create users table
 CREATE TABLE users (
-	users_id SMALLINT UNSIGNED NOT NULL auto_increment,
+	id SMALLINT UNSIGNED NOT NULL auto_increment,
 
 	username VARCHAR(255) NOT NULL,
 	password_hash VARCHAR(128) NOT NULL,
 
-	CONSTRAINT pk_users PRIMARY KEY (users_id)
+	CONSTRAINT pk_users PRIMARY KEY (id)
 );
