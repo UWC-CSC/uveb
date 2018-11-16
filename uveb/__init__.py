05cb1de9b2d89459a62a781da88e11d60b9a5306
Marko Vejnovic <marko.vejnovic@hotmail.com>
2018-11-16 16:11:57 +0800

    Added CORS support

--- uveb/__init__.py
+++ uveb/__init__.py
@@ -42,5 +42,15 @@ def initialize():
     controllers.init(connection)
 
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers',
+            'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods',
+            'GET,PUT,POST,DELETE')
+    return response
+
+
 if __name__ == '__main__':
     app.run(debug=True)
